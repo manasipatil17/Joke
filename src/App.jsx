7afcf980c8709b233793f63177c8c4d3c7f06e70
abhishekpatil9095 <132066200+abhishekpatil9095@users.noreply.github.com>
2024-10-10 11:35:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Jokes from "./Jokes";
+function App() {
+  return (
+    <>
+      <div>
+       <Jokes/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Jokes.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+export default function Jokes(){
+    let [joke, setJoke] = useState({});
+
+    const URL= "https://official-joke-api.appspot.com/random_joke";
+    const getNewJoke = async () =>{
+        let response = await fetch(URL);
+        let jsonResponce = await response.json();
+        setJoke({setup: jsonResponce.setup, punchline: jsonResponce.punchline});
+        
+    }
+
+    useEffect(() => {
+        async function getFirstJoke(){
+        let response = await fetch(URL);
+        let jsonResponce = await response.json();
+        console.log(jsonResponce);
+        setJoke({setup: jsonResponce.setup, punchline: jsonResponce.punchline});
+        }
+        getFirstJoke();
+    }, []);
+
+    return(
+        <div>
+            <h3>Jokes!</h3>
+            <h2>{joke.setup}</h2>
+            <h2>{joke.punchline}</h2>
+            <button onClick={getNewJoke}>New Joke</button>
+        </div>
+    );
+}
