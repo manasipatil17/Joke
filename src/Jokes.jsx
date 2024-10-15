75a2b58bb6e9fc909da048a98be2e8dfe9466326
Manasi Patil <manasipatil17052004@gmail.com>
2024-10-15 12:42:53 +0530

    check

--- src/Jokes.jsx
+++ src/Jokes.jsx
@@ -24,7 +24,6 @@ export default function Jokes(){
     return(
         <div>
             <h3>Jokes!</h3>
-            <h3>Manasi Patil</h3>
             <h2>{joke.setup}</h2>
             <h2>{joke.punchline}</h2>
             <button onClick={getNewJoke}>New Joke</button>
